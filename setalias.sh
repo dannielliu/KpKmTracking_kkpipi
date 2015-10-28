1e4a8b086394201feb677add77aa44e5c5a3ff3c
danny <liudongzy@163.com>
2015-10-28 14:15:39 +0800

    new file:   setalias.sh

---
+++ setalias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+alias Tracking_dataKp="./KaonTrack /Volumes/data2/besIIIdata/KaTrk_KKPiPi/data/KaTrk_Kp/combined/Tracking_KKPiPi_30800.root /Volumes/data2/besIIIdata/KaTrk_KKPiPi/data/KaTrk_Kp/combined/Tracking_KKPiPi_29000.root"
+
